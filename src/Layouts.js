503e819bb7d36811f52b87b816199fdb18765455
nisticism <nistacular@gmail.com>
2020-09-11 17:06:24 -0700

    Added backend api, card class

---
+++ src/Layouts.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CardLayout = styled.div`
+  height: 100px;
+  width: 50px;
+  background-color: red;
+`;
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { CardLayout } from "../Layouts";
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        <CardLayout />
+      </div>
+    );
+  }
+}
